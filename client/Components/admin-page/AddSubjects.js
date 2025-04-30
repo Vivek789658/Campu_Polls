2f9afefad3bdbaa62932825443c54007dfde44c6
Vivek Kumar <128015766+Vivek789658@users.noreply.github.com>
2025-04-30 14:31:00 +0530

    Update AddSubjects.js

--- client/Components/admin-page/AddSubjects.js
+++ client/Components/admin-page/AddSubjects.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import Alert from "../common/Alert";
 require("dotenv").config();
-const BASE_URL = "https://campu-polls.onrender.com";
+const BASE_URL = "https://pr-01.onrender.com";
 
 const AddSubjects = () => {
   const [file, setFile] = useState(null);
